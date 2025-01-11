c2aa10f1bf82029685ba70e9969a32eb0af0551a
Izabela Krzyżowska <152220665+numname869@users.noreply.github.com>
2025-01-11 14:58:50 +0100

    Update ToCheck.sql

--- Database/Queries/ToCheck.sql
+++ Database/Queries/ToCheck.sql
@@ -1,13 +1,13 @@
+DECLARE @LoginToCheck NVARCHAR(255) = 'test_user';
+DECLARE @PasswordToCheck NVARCHAR(255) = 'test_password';
 
-DECLARE @LoginToCheck NVARCHAR(255) = 'login';
-
+-- Sprawdzanie, czy login istnieje
 IF EXISTS (SELECT 1 FROM Konta WHERE Login = @LoginToCheck)
     SELECT 'TRUE' AS LoginExists
 ELSE
     SELECT 'FALSE' AS LoginExists;
 
-DECLARE @PasswordToCheck NVARCHAR(255) = 'haslo';
-
+-- Sprawdzanie, czy login i hasło pasują
 IF EXISTS (SELECT 1 FROM Konta WHERE Login = @LoginToCheck AND [Hasło] = @PasswordToCheck)
     SELECT 'TRUE' AS LoginPasswordMatch
 ELSE
