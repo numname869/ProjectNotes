fbd3c210ef359c68ef7c8aaa214ec9645b20dbe9
faustyna kula <wolfress3@gmail.com>
2025-01-11 15:41:15 +0100

    poprawki

--- zooklientserver/ZooServer/obj/Debug/net8.0/ZooServer.AssemblyInfo.cs
+++ zooklientserver/ZooServer/obj/Debug/net8.0/ZooServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1a46e9a47800d919c9e71ba95e4bc55698bad8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2963f0e94868678e9e7d869633d63f8a956fbc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
