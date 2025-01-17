c94b3cd3ffb7437138496ce446e9bbf14df0757a
Daria <148493557+Venatrix02@users.noreply.github.com>
2025-01-17 22:56:48 +0100

    Added Program Main Class

--- ZooProjekt/Program.cs
+++ ZooProjekt/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace ZooProjekt
+﻿using System;
+
+namespace ZooProjekt
 {
     internal class Program
     {
-        
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            app.Run();
+        }
+
     }
 }
